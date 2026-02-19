378965534e3cc6301c900c58c5be40e5a86ca789
JoemanJ <diogodelazare@hotmail.com>
2026-02-19 16:09:33 -0300

    Added slug to post model and meta class tor Profile model

--- backend/desabafeAqui/blog/models.py
+++ backend/desabafeAqui/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import secrets
+import string
+
 def user_profile_picture_path(instance, filename) -> str:
     """ 
     Helper function to define the upload path  for user profile pictures.
-    The default path is 'profile_pictures/[username].
+    The default path is 'profile_pictures/[username's id].
     """
     return f"profile_pictures/{instance.user.pk}/{filename}"
 
@@ -30,7 +33,15 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s (id: {self.user.pk}) profile"
     
+    class Meta:
+        ordering = ['user']
+        verbose_name = "User profile"
+        verbose_name_plural = "User profiles"
+    
 class Post(models.Model):
+    """
+    A post made by a user.
+    """
     author = models.ForeignKey(User, 
                              verbose_name=("The user that made the post"), 
                              on_delete=models.CASCADE,
@@ -51,9 +62,35 @@ class Post(models.Model):
                                        auto_now=True,
                                        help_text="Data da última edição do desabafo",
                                        db_comment="Date the post was last edited")
+    
+    slug = models.SlugField(max_length=8, 
+                            unique=True, 
+                            blank=True,
+                            db_comment="Unique, random, small id for the post")
 
     def __str__(self):
         return self.text
+    
+    def save(self, *args, **kwargs):
+        """
+        If the post doesn't have a slug yet, creates a random one before saving
+        """
+        if not self.slug:
+            self.slug = self.generate_unique_slug()
+        super().save(*args, **kwargs)
+
+    def generate_unique_slug(self):
+        """
+        Generates a unique 16-digit slug for the post
+        """
+        characters = string.ascii_letters + string.digits
+        new_slug = ''.join(secrets.choice(characters) for _ in range(8))
+        
+        # Guard against duplicate slugs
+        if Post.objects.filter(slug=new_slug).exists():
+            return self.generate_unique_slug()
+        
+        return new_slug
 
     class Meta:
         ordering = ['-created_at']
