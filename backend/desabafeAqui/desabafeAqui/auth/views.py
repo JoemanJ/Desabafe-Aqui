1de1e19e718b34634fe7fc639ce750b8a0745e09
JoemanJ <diogodelazare@hotmail.com>
2026-03-03 20:28:21 -0300

    added username to JWT

---
+++ backend/desabafeAqui/desabafeAqui/auth/views.py
@@ -0,0 +1,5 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializer import MyTokenObtainPairSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
--- backend/desabafeAqui/desabafeAqui/urls.py
+++ backend/desabafeAqui/desabafeAqui/urls.py
@@ -19,16 +19,18 @@ from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from drf_spectacular.views import (SpectacularAPIView, 
                                    SpectacularSwaggerView, 
                                    SpectacularRedocView)
 
+from .auth.views import MyTokenObtainPairView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('api/', include('blog.urls')),    
 
     # API documentation
