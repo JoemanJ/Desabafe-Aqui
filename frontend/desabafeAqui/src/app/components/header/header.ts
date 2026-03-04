f67f1f6cc13c1bde3a2c8f0546663716b713d268
JoemanJ <diogodelazare@hotmail.com>
2026-03-04 17:06:28 -0300

    Login button now hidden in login page

--- frontend/desabafeAqui/src/app/components/header/header.ts
+++ frontend/desabafeAqui/src/app/components/header/header.ts
@@ -1,5 +1,7 @@
 import { Component, inject, input } from '@angular/core';
 import { LoginOrProfile } from './login-or-profile/login-or-profile';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +10,15 @@ import { LoginOrProfile } from './login-or-profile/login-or-profile';
   styleUrl: './header.scss',
 })
 export class Header {
+  private router = inject(Router);
+
+  showLoginProfileSection = true;
+
+  constructor(){
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe((e: NavigationEnd) => {
+      this.showLoginProfileSection = e.urlAfterRedirects !== '/login'
+    })
+  }
 }
